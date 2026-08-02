64451cbf8adc35ff03550efd0387a774a8a572e4
kryptodel <danilo43278oskdj@gmail.com>
2026-08-02 12:08:31 -0300

    Update batmobile.py

--- batmobile.py
+++ batmobile.py
@@ -9,7 +9,7 @@ async def prepare_batmobile(channel_or_interaction, name: str, is_slash=False):
     )
     embed.set_footer(text="Alfred Pennyworth • Always at your service")
 
-    file = discord.File("batmobile.gif", filename="batmobile.gif")
+    file = discord.File("batman-batmóvel.gif", filename="batman-batmóvel.gif)
     embed.set_image(url="attachment://batmobile.gif")
 
     if is_slash:
